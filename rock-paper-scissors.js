2056d5082db45ff7f5da6795c782bb503c64dfe6
Francis Chryss Pegenia <chrysspegenia@gmail.com>
2023-05-15 11:04:50 +0800

    Remove gameRepeater and game functions as unessential

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,17 +1,17 @@
 let playerSelection;
-let computerSelection = getComputerChoice();
+let computerSelection;
 let computerScore = 0;
 let playerScore = 0;
 const buttons = document.querySelectorAll(".button");
 
 const player = document.querySelector(".playerScore");
-player.textContent = `${playerScore}`;
+
 
 const computer = document.querySelector(".computerScore");
-computer.textContent = `${computerScore}`;
+
 
 const announcement = document.querySelector(".announcer");
-announcement.textContent = announcer();
+announcement.textContent = playRound(playerSelection, computerSelection);
 
 
 //TRIAL
@@ -24,6 +24,12 @@ buttons.forEach(button => button.addEventListener("click", () => {
     playerSelection = button.id;
     getComputerChoice();
     playRound(playerSelection, computerSelection);
+    //announcer();
+    
+
+    player.textContent = `${playerScore}`;
+    computer.textContent = `${computerScore}`;
+    announcement.textContent = playRound(playerSelection, computerSelection);
 
     //temporary
     playerText.textContent = `Player: ${playerSelection}`;
@@ -58,52 +64,61 @@ buttons.forEach(button => button.addEventListener("click", () => {
 
 
 //This function outputs a random choice of rock, paper,or scissors
+//function getComputerChoice(){
+//    let choices = ["rock", "paper", "scissors"];
+//    let randomChoice = Math.floor(Math.random()*choices.length);
+//    let randomResult = choices[randomChoice];
+//    return randomResult;
+//}
+
 function getComputerChoice(){
-    let choices = ["rock", "paper", "scissors"];
-    let randomChoice = Math.floor(Math.random()*choices.length);
-    let randomResult = choices[randomChoice];
-    return randomResult;
+    const randomChoice = Math.floor(Math.random() * 3)+1;
+
+    switch(randomChoice){
+        case 1:
+            computerSelection = "rock";
+        break;
+        case 2:
+            computerSelection = "paper";
+        break;
+        case 3:
+            computerSelection = "scissors"
+        break;
+    }
 }
 
 
-//this function compares the userInput and getComputerChoice and returns an appropriate response.
+
+//this function compares the playerSelection and getComputerChoice and returns an appropriate response.
 //added code that will add 1 point to whoever won a round.
 function playRound(playerSelection, computerSelection){
-    
+
     let roundResult;
     if (playerSelection === "rock" && computerSelection === "rock"){
             roundResult = "It's a Draw! You both drew Rock."
         } else if (playerSelection === "rock" && computerSelection === "paper"){
             roundResult = "You Lose! Your Rock loses to Paper.";
-            computerScore += 1; 
+            computerScore++; 
         } else if (playerSelection === "rock" && computerSelection === "scissors"){
             roundResult = "You Win! Your Rock beats Scissors."; 
-            playerScore += 1;
+            playerScore++;
         } else if (playerSelection === "paper" && computerSelection === "rock"){
             roundResult = "You Win! Your Paper beats Rock.";
-            playerScore += 1+playerScore;
+            playerScore++;
         } else if (playerSelection === "paper" && computerSelection === "paper"){
             roundResult = "It's a Draw! You both drew Paper."
         } else if (playerSelection === "paper" && computerSelection === "scissors"){
             roundResult = "You Lose! Your Paper loses to Scissors.";
-            computerScore += 1;
+            computerScore++;
         } else if (playerSelection === "scissors" && computerSelection === "rock"){
             roundResult = "You Lose! Your Scissors loses to Rock.";
-            computerScore += 1;
+            computerScore++;
         } else if (playerSelection === "scissors" && computerSelection === "paper"){
             roundResult = "You Win! Your Scissors beats Paper.";
-            playerScore += 1; 
-        } else if (playerSelection ==="scissors" && computerSelection === "scissors") {
-            roundResult = "It's a Draw! You both drew Scissors."
+            playerScore++; 
         } else {
-            alert("Please enter rock, paper or scissors.")
-            //this part prompts the user again they did not input rock, paper or scissors
-            //if (playerSelection !== "rock" || playerSelection !== "paper" || playerSelection !== "scissors"){
-            //    prompt(userInput)
-            //} else {
-
-            //}
-        }
+            roundResult = "It's a Draw! You both drew Scissors."
+        };
 
         return roundResult;
 }
@@ -112,24 +127,46 @@ function playRound(playerSelection, computerSelection){
 //displays a message whenever the player or computer scores
 function announcer(){
     let announcement;
+
+//    if (playerScore == 0 && computerScore == 0){
+//        announcement = "Let's play.";
+//    } else if (playerScore == 5){
+//        announcement = "You're the winner.";
+//        playerScore = 0;
+//        computerScore = 0;
+//    } else if (computerScore == 5){
+//        announcement = "You lost.";
+ //       playerScore = 0;
+ //       computerScore = 0;
+ //   } else if (playerScore > computerScore){
+ //       announcement = "You won the round."
+ //   } else if (computerScore > playerScore){
+ //       announcement = "You lost this round."
+ //   } else {
+ //       announcement = "It's a tie."
+ //   }
+
+
+
+
     if (playerScore === 1){
         announcement = "Good Job."
-    } else if (playerScore === 2){
-        announcement = "Alright! You'll win if you keep at it."
-    } else if (playerScore === 3){
-        announcement = "*Chandler celebration dance*"
-    } else if (playerScore === 4){
-        announcement = "One more to go."
-    } else if (playerScore === 5){
-        announcement = "You've done it. You won!"
     } else if (computerScore === 1){
         announcement = "That's one for the computer."
+    } else if (playerScore === 2){
+        announcement = "Alright! You'll win if you keep at it."
     } else if (computerScore === 2){
         announcement = "The computer is really going for it"
+    } else if (playerScore === 3){
+        announcement = "*Chandler celebration dance*"
     } else if (computerScore === 3){
         announcement = "This computer is good. I wonder if its cheating?"
+    } else if (playerScore === 4){
+        announcement = "One more to go."
     } else if (computerScore === 4){
         announcement = "The computer only needs 1 more win."
+    } else if (playerScore === 5){
+        announcement = "You've done it. You won!"
     } else if (computerScore === 5){
         announcement = "The computer won the match."
     } else if (playerScore === computerScore){
@@ -140,24 +177,6 @@ function announcer(){
 
     return `${announcement}`;
 }
-
-//to be tweeked or might delete
-//function game(){
-//    let winnerAnnouncement;
-//    if (playerScore === 2) {
-//        winnerAnnouncement = "You have won the game."
-//    } else if (computerScore === 2){
-//        winnerAnnouncement = "The computer won the game."
-//    } else {
-//        winnerAnnouncement = "The fight continues"
-//    }
-
-//    return winnerAnnouncement;
-//}
-
-
-//let rockBtn = document.querySelector("#rock")
-
 
 
 //const computerSelection = getComputerChoice();
