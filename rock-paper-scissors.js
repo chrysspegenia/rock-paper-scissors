edeeef2b4c82b9d19ac68ac1e0a5f59ed57cd073
Francis Chryss Pegenia <chrysspegenia@gmail.com>
2023-05-23 11:45:31 +0800

    Remove announcer(), unused and commented codes

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -70,59 +70,6 @@ function playRound(playerSelection, computerSelection){
 };
 
 
-//displays a message whenever the player or computer scores
-function announcer(){
-    let announcement;
-
-//    if (playerScore == 0 && computerScore == 0){
-//        announcement = "Let's play.";
-//    } else if (playerScore == 5){
-//        announcement = "You're the winner.";
-//        playerScore = 0;
-//        computerScore = 0;
-//    } else if (computerScore == 5){
-//        announcement = "You lost.";
- //       playerScore = 0;
- //       computerScore = 0;
- //   } else if (playerScore > computerScore){
- //       announcement = "You won the round."
- //   } else if (computerScore > playerScore){
- //       announcement = "You lost this round."
- //   } else {
- //       announcement = "It's a tie."
- //   }
-
-
-
-
-    if (playerScore === 1){
-        announcement = "Good Job."
-    } else if (computerScore === 1){
-        announcement = "That's one for the computer."
-    } else if (playerScore === 2){
-        announcement = "Alright! You'll win if you keep at it."
-    } else if (computerScore === 2){
-        announcement = "The computer is really going for it"
-    } else if (playerScore === 3){
-        announcement = "*Chandler celebration dance*"
-    } else if (computerScore === 3){
-        announcement = "This computer is good. I wonder if its cheating?"
-    } else if (playerScore === 4){
-        announcement = "One more to go."
-    } else if (computerScore === 4){
-        announcement = "The computer only needs 1 more win."
-    } else if (playerScore === 5){
-        announcement = "You've done it. You won!"
-    } else if (computerScore === 5){
-        announcement = "The computer won the match."
-    } else if (playerScore === computerScore){
-        announcement = "Let's keep going the match is'nt over"
-    } 
-
-    return `${announcement}`;
-}
-
-
 function gameResult(){
     if(playerScore === 5 || computerScore === 5){
         openPopup();
@@ -279,24 +226,6 @@ function roundResultColor(){
     } 
 }
 
-//just for fun
-// document.addEventListener("mousemove", (e) => {
-//     const mouseX = e.clientX;
-//     const mouseY = e.clientY;
-
-//     const mortyEyes = document.getElementById("mortyEyes");
-//     const rekt = mortyEyes.getBoundingClientRect();
-//     const mortyEyesX = rekt.left + rekt.width / 2;
-//     const mortyEyesY = rekt.top + rekt.height / 2;
-
-//     const angleDeg = angle(mouseX, mouseY, mortyEyesX, mortyEyesY);
-
-//     const eyes = document.querySelectorAll(".eye");
-//     eyes.forEach(eye => {
-//         eye.style.transform = `rotate(${90 + angleDeg}deg)`;
-//         mortyEyes.style.filter = `hue-rotate(${angleDeg}deg)`;
-//     })
-// })
 
 document.addEventListener("mousemove", (e) => {
     const mouseX = e.clientX;
@@ -333,11 +262,3 @@ function angle(cx, cy, ex, ey){
     const deg = rad * 180 / Math.PI;
     return deg;
 }
-//const computerSelection = getComputerChoice();
-//const computerSelection = "scissors";
-//console.log("computer: " + computerSelection);
-//console.log(playRound(playerSelection, computerSelection));
-//console.log(`Rounds the player has won: ${playerScore}`);
-//console.log(`Rounds the computer has won: ${computerScore}`);
-
-//console.log(announcer());
